f11353ac257277ca7cfc1ff8e96c7cbaecfb1719
Long <long59915@gmail.com>
2022-05-27 12:00:57 +0700

    27/05

--- src/main/java/com/dvhl/forum_be/Security/WebSecurityConfig.java
+++ src/main/java/com/dvhl/forum_be/Security/WebSecurityConfig.java
@@ -67,7 +67,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers("/register").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/files/**").permitAll()
