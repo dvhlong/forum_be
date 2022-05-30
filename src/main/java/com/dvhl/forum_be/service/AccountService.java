b909f6035339e22fdf926e024eeb8018c285e075
Long <long59915@gmail.com>
2022-05-30 13:14:16 +0700

    30/05

--- src/main/java/com/dvhl/forum_be/service/AccountService.java
+++ src/main/java/com/dvhl/forum_be/service/AccountService.java
@@ -198,10 +198,11 @@ public class AccountService {
                             FileContent mediaContent = new FileContent("image/jpeg", newfileRename);
                             File newGGDriveFile = new File();
                             newGGDriveFile.setName(fileRename);
-                            newGGDriveFile.setParents(ImmutableList.of("1nHgSB-J0xYvQJS8awH9EWakjX5vU-RYm"));
+                            // newGGDriveFile.setParents(ImmutableList.of("1nHgSB-J0xYvQJS8awH9EWakjX5vU-RYm"));
                             if (user.getAvatar() != null) {
-                                googleDrive.files().delete(user.getAvatar()).execute();
-                                upload(user, mediaContent, newGGDriveFile);
+                                // googleDrive.files().delete(user.getAvatar()).execute();
+                                // upload(user, mediaContent, newGGDriveFile);
+                                googleDrive.files().update(user.getAvatar(), newGGDriveFile, mediaContent).execute();
                             } else {
                                 upload(user, mediaContent, newGGDriveFile);
                             }
@@ -228,6 +229,7 @@ public class AccountService {
     }
 
     private void upload(User user, FileContent mediaContent, File newGGDriveFile) throws IOException {
+        newGGDriveFile.setParents(ImmutableList.of("1nHgSB-J0xYvQJS8awH9EWakjX5vU-RYm"));
         File file = googleDrive.files().create(newGGDriveFile, mediaContent)
                 .setFields("id")
                 .execute();
